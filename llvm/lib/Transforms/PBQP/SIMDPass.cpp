5aff9baac29afe0762688c5bec297b03baa1894d
KAKUDO Kentaro <31888089+kakudo415@users.noreply.github.com>
2024-02-03 00:10:34 +0900

    refactor: Separate into another function

--- llvm/lib/Transforms/PBQP/SIMDPass.cpp
+++ llvm/lib/Transforms/PBQP/SIMDPass.cpp
@@ -5,17 +5,20 @@ using namespace llvm;
 
 #define DEBUG_TYPE "SIMD Instruction Inserter by PBQP"
 
+static void addPossibleSIMDInstructions(BasicBlock &BB) {
+  for (Instruction &I : BB) {
+    errs() << I.getOpcodeName() << '\n';
+  }
+}
+
 namespace {
 struct PBQPSIMD : public FunctionPass {
   static char ID;
   PBQPSIMD() : FunctionPass(ID) {}
 
   bool runOnFunction(Function &F) override {
-    errs() << "Function: " << F.getName() << '\n';
-    for (Function::iterator bb = F.begin(); bb != F.end(); bb++) {
-      for (BasicBlock::iterator i = bb->begin(); i != bb->end(); i++) {
-        errs() << i->getOpcodeName() << '\n';
-      }
+    for (BasicBlock &BB : F) {
+      addPossibleSIMDInstructions(BB);
     }
     return false;
   }
